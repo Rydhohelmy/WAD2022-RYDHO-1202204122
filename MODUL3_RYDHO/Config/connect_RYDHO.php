7a8c9acfc54a9b1c459170573f429c15cad5e277
Rydhohelmy <115517658+Rydhohelmy@users.noreply.github.com>
2022-11-20 18:08:35 +0700

    MODUL3_RYDHO

---
+++ MODUL3_RYDHO/Config/connect_RYDHO.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "Showroom_RYDHO_table";
+
+$conn = new mysqli($servername, $username, $password, $database);
+$connect = mysqli_connect($servername, $username, $password, $database);
+
+?>
